88f6921ddd5ca4f9dc64df0964d979977b6f656a
perchespierri <perchespierri@gmail.com>
2022-04-18 13:53:31 -0300

    feat: Create POST route for customer creation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = 3333;
+const customers = [];
 
-app.listen(PORT)
+app.use(express.json())
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  
+  const customerAlreadyExists = customers.some(
+    customer => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ 
+      message: "Customer already exists"
+    })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send()
+});
+
+app.listen(PORT);

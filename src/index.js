57f3c2d0618ae9230509aaf1076402d47aa69feb
perchespierri <perchespierri@gmail.com>
2022-04-18 19:19:43 -0300

    feat: get route using header

--- src/index.js
+++ src/index.js
@@ -30,4 +30,15 @@ app.post("/account", (request, response) => {
   return response.status(201).send({message: "ACCOUNT CREATED"})
 });
 
+app.get("/statement/", (request, response) => {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: "CUSTOMER NOT FOUND" });
+  }
+  
+  return response.json(customer.statement);
+});
+
 app.listen(PORT);
